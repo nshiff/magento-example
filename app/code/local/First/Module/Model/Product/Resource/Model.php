a0ada2b6813ab5291cb106908daae321a36b2169
Nate S <nshiff@users.noreply.github.com>
2015-12-18 13:19:03 -0500

    adding a rewrite for an eav resource model

---
+++ app/code/local/First/Module/Model/Product/Resource/Model.php
@@ -0,0 +1,7 @@
+<?php
+
+
+
+class First_Module_Model_Product_Resource_Model extends Mage_Catalog_Model_Resource_Eav_Mysql4_Product{
+	
+}
--- test.php
+++ test.php
@@ -4,23 +4,25 @@
 include 'app/Mage.php';
 Mage::app();	//Mage/core/Model/App
 
-$object_example_model = Mage::getModel('catalog/product');
 
-echo(get_class($object_example_model));
+$object_model = Mage::getModel('catalog/product');
+echo(get_class($object_model));
+echo('<br>');
+echo($object_model->getName());
+echo('<br>');
 echo('<br>');
 
-echo($object_example_model->getName());
-echo('<br>');
-echo('<br>');
-
-$object = Mage::app()
+$object_product = Mage::app()
 	->getLayout()
 	->createBlock('catalog/product_view');
-
-echo(get_class($object));
+echo(get_class($object_product));
+echo('<br>');
 echo('<br>');
 
-
+$object_resource_model = Mage::getResourceModel('catalog/product');
+echo(get_class($object_resource_model));
+echo('<br>');
+echo('<br>');
 
 
 
