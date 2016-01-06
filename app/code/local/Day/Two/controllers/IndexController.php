0aa47ff758ddaa0f6859d3d6e1e2f7e578a41abe
Nate S <nshiff@users.noreply.github.com>
2016-01-06 10:52:05 -0500

    practice logging, and peeking at structure of layout xml

--- app/code/local/Day/Two/controllers/IndexController.php
+++ app/code/local/Day/Two/controllers/IndexController.php
@@ -5,26 +5,38 @@
 class Day_Two_IndexController extends Mage_Core_Controller_Front_Action{	//"IndexController" is a default name
 																			// also it is the filename
 	public function indexAction(){	// route 'heyo' hits default action
-		
 		echo('Hello, world!');
 		echo('<br>');
-		
 	}
 	
 	public function modelAction(){	// route 'heyo/index/model'
-		
 		echo('hi');
 		echo('<br>');
 		echo(get_class($this));
 		echo(get_class(Mage::getModel('day_two/whatever')));		
+	}
+	
+	public function layoutAction(){	// route 'heyo/index/layout'
+		$xml = $this->loadLayout()->getLayout()->getUpdate()->asString();
+		
+		$this->getResponse()->setHeader('Content Type', 'text/plain')->setBody($xml);
+		
+		$forceLog = true;
+		Mage::log('Exercise: 03 Request Flow 06 Design and Layout Initialization: Magento U', Zend_Log::INFO, 'layout.log', $forceLog);
+		Mage::log($xml, Zend_Log::INFO, 'layout.log', $forceLog);
+		
 		
 	}
 	
 	
-	public function layoutAction(){	// route 'heyo/index/layout'
+	public function defaultAction(){
 		
-		echo('heyo/index/layout');
+		echo('welcome...');
 		
+		$this->loadLayout()->renderLayout();
+		
+		echo('<br>...to The Matrix.');
+	
 	}
 	
 	
